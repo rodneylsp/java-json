00538603a466e24a20c9ed26324c6fb8c73e1e22
y9h5 <y9h5@MI00278956.petrobras.biz>
2017-10-18 18:13:45 -0200

    Adicionando tabela e formulario de entrada [parcial]

--- angularjs-agenda/js/agenda.js
+++ angularjs-agenda/js/agenda.js
@@ -1,3 +1,18 @@
 /**
  * 
  */
+
+angular.module("agendaApp", []);
+angular.module("agendaApp").controller("agendaController", function ($scope){
+	
+	$scope.contato = {nome:"Rodney", telefone:"9986-8441"}
+	
+	$scope.contatos = [
+	                   {nome:"Joao", telefone:"9999-6666"},
+	                   {nome:"Maria", telefone:"9999-8888"},
+	                   {nome:"Ana", telefone:"9999-2222"}
+	];
+	
+	
+	
+});
