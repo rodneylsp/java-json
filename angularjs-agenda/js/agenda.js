31f329166e4b2d49e46c9d29f9c54826fc94bafb
Unknown <rodneylsp@gmail.com>
2017-10-19 00:01:59 -0200

    Adicionando campos no contato
    Adição do campo operadora e cor a contato e testando as diretivas ng-message.

--- angularjs-agenda/js/agenda.js
+++ angularjs-agenda/js/agenda.js
@@ -1,18 +1,44 @@
 /**
- * 
+ *
  */
 
-angular.module("agendaApp", []);
+angular.module("agendaApp", ["ngMessages"]);
 angular.module("agendaApp").controller("agendaController", function ($scope){
-	
-	$scope.contato = {nome:"Rodney", telefone:"9986-8441"}
-	
+
+	$scope.contato = undefined;
+
 	$scope.contatos = [
-	                   {nome:"Joao", telefone:"9999-6666"},
-	                   {nome:"Maria", telefone:"9999-8888"},
-	                   {nome:"Ana", telefone:"9999-2222"}
+   {nome:"Joao", telefone:"9999-6666", cor:"yellow"},
+   {nome:"Maria", telefone:"9999-8888", cor:"blue"},
+   {nome:"Ana", telefone:"9999-2222", cor:"red"}
 	];
-	
-	
-	
+
+	$scope.operadoras = [
+		{nome: "Oi", codigo:31, categoria: "Celular"},
+		{nome: "Vivo", codigo:14, categoria: "Celular"},
+		{nome: "Tim", codigo:21, categoria: "Fixo"}
+	];
+
+	$scope.classe1 = "selecionado";
+	$scope.classe2 = "negrito";
+
+	$scope.adicionarContato = function(contato){
+		$scope.contatos.push(angular.copy(contato));
+		delete $scope.contato;
+		$scope.contatoForm.$setPristine();
+	};
+
+	$scope.apagarContato = function(contatos){
+		$scope.contatos = contatos.filter(function (contato){
+			if(!contato.selecionado) return contato;
+		});
+	};
+
+	$scope.isContatoSelecionado = function(contatos) {
+		return contatos.some(function (contato){
+			return contato.selecionado;
+		});
+	};
+
+
 });
